dbc6a3521da8c92fc40acde61888d2f88e242d05
Acesso Livre <Acesso Livre@BC7N278816>
2026-07-06 18:59:17 -0300

    bff

--- bibliotecaestantemagica/src/main/java/br/com/empresa18/integracao/bibliotecaestantemagica/repository/EmprestismoRepository.java
+++ bibliotecaestantemagica/src/main/java/br/com/empresa18/integracao/bibliotecaestantemagica/repository/EmprestismoRepository.java
@@ -4,6 +4,7 @@ package br.com.empresa18.integracao.bibliotecaestantemagica.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import br.com.empresa18.integracao.bibliotecaestantemagica.entity.EmprestimoEntity;
 
@@ -13,5 +14,10 @@ public interface EmprestismoRepository extends JpaRepository<EmprestimoEntity, L
 	List<EmprestimoEntity> findByUsuarioId(Long id);
 	List<EmprestimoEntity> findByUsuarioRa(String ra);
 
-
+		@Query(	"SELECT \r\n"
+				+ "FROM Emprestimo\r\n"
+				+ "WHERE DATEADD(DAY, 10, dataEmprestimo) < GETDATE()"
+		)
+		List<EmprestimoEntity> findEmprestimoAtrasados();
+			
 }
