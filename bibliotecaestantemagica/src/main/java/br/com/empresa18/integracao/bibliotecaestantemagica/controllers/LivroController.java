cb2611c2c78577f95e3d08298e82d9b805b4a05d
Acesso Livre <Acesso Livre@BC7N278816>
2026-07-09 22:11:28 -0300

    atualização da mina busca de livros

--- bibliotecaestantemagica/src/main/java/br/com/empresa18/integracao/bibliotecaestantemagica/controllers/LivroController.java
+++ bibliotecaestantemagica/src/main/java/br/com/empresa18/integracao/bibliotecaestantemagica/controllers/LivroController.java
@@ -172,6 +172,12 @@ public class LivroController {
 		}
 	
 	
+	@GetMapping("/BuscarPorTitulo/{titulo}")
+	@ResponseStatus(HttpStatus.OK)	    
+	public List<LivroEntity> buscarPorTitulo(@PathVariable String titulo) {
+	    return livroRepo.findByTituloContainingIgnoreCase(titulo);
+	}
+	
 
 }
 
