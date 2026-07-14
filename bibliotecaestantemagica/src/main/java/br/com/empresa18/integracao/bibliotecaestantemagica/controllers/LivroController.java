4dfaa4d88499632b41fd887a1ee4b26a39d182c8
Acesso Livre <Acesso Livre@BC7N278819>
2026-07-14 19:08:48 -0300

    Evandro

--- bibliotecaestantemagica/src/main/java/br/com/empresa18/integracao/bibliotecaestantemagica/controllers/LivroController.java
+++ bibliotecaestantemagica/src/main/java/br/com/empresa18/integracao/bibliotecaestantemagica/controllers/LivroController.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-=======
-import org.springframework.web.bind.annotation.*;
->>>>>>> branch 'master' of https://github.com/JenifferVitoria/biblioteca.git
-
 import br.com.empresa18.integracao.bibliotecaestantemagica.entity.LivroEntity;
 import br.com.empresa18.integracao.bibliotecaestantemagica.repository.LivroRepository;
 
@@ -193,20 +187,13 @@ public class LivroController {
 	       }
 			return "Não Salvo";
 		}
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/JenifferVitoria/biblioteca.git
-	
 	
 	@GetMapping("/BuscarPorTitulo/{titulo}")
 	@ResponseStatus(HttpStatus.OK)	    
 	public List<LivroEntity> buscarPorTitulo(@PathVariable String titulo) {
 	    return livroRepo.findByTituloContainingIgnoreCase(titulo);
 	}
-<<<<<<< HEAD
-	
-=======
+
 
 	
 	@PostMapping("/reservar/{id}")
@@ -230,9 +217,7 @@ public class LivroController {
 	    return "Livro reservado com sucesso!";
 	}
 
->>>>>>> branch 'master' of https://github.com/JenifferVitoria/biblioteca.git
 }
-
 
 
 
